fabb6b096f2fe9972c8f32699074a3dc5206d0a9
Oliver Mansell <olivermansell@Olivers-MacBook-Pro.local>
2018-04-16 14:49:22 +0100

    update bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+
+echo 'Setting env variable...'
+export REACT_APP_USERS_SERVICE_URL=http://127.0.0.1:5001
+
+wait
+echo 'Building containers...'
+docker-compose -f docker-compose-dev.yml build
+
+
+wait
+echo 'Install local packages...'
+cd services/client && yarn install
+
+wait
+cd ../..
+echo 'Recreating Database...'
+docker-compose -f docker-compose-dev.yml run users-service python manage.py recreate_db
+
+wait
+echo 'Seeding database...'
+docker-compose -f docker-compose-dev.yml run users-service python manage.py seed_db
+
+
+echo 'Spinning up containers...'
+docker-compose -f docker-compose-dev.yml up
